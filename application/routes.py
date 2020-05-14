ec136a295530ff8e364db534dee53f3f646d6cf4
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-14 22:07:46 +0100

    route for index created

--- application/routes.py
+++ application/routes.py
@@ -1,2 +1,6 @@
 from application import app
 from flask import render_template, url_for
+
+@app.route("/")
+def index():
+    return render_template("index.html", page_title="Welcome to Travelbuddy portal")
