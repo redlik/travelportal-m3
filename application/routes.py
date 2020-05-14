e92ca57ce480b1b1dacf3d803117ed13db979a37
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-14 22:48:20 +0100

    test slugify to generate url friendly strings

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,14 @@
 from application import app
 from flask import render_template, url_for
+from slugify import slugify
 
 @app.route("/")
 def index():
     return render_template("index.html", page_title="Welcome to Travelbuddy portal")
+
+@app.route("/tour")
+def tour_name():
+    tour = "Paris 7 days"
+    tour_name = slugify(tour)
+    return render_template("tour.html", tour_name = tour_name)
+
