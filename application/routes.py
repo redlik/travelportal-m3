a61c58052015ced64e6ade6490ab2ba801dacdf9
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-15 15:35:12 +0100

    Navigation bar added to base template

--- application/routes.py
+++ application/routes.py
@@ -4,9 +4,9 @@ from slugify import slugify
 
 @app.route("/")
 def index():
-    user_collection = database.db.users
-    user_collection.insert({'name': 'John'})
-    return render_template("index.html", page_title="Welcome to Travelbuddy portal")
+    # user_collection = database.db.users
+    # user_collection.insert({'name': 'John'})
+    return render_template("index.html", page_title="Welcome to Travelbuddy portal", home_page=True)
 
 @app.route("/tour")
 def tour_name():
@@ -14,3 +14,14 @@ def tour_name():
     tour_name = slugify(tour)
     return render_template("tour.html", tour_name = tour_name)
 
+@app.route('/tours')
+def tours():
+    return render_template('tours.html', tours_page=True)
+
+@app.route('/register')
+def register():
+    return render_template('register.html', register_page=True)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', login_page=True)
