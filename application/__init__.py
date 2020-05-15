1f979e15b1ae062ec7a504cf5e63a5b3ae6cf173
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-15 14:58:41 +0100

    Linked Mongo database to application

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, instance_relative_config=True)
+# app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+app.config["MONGO_URI"] = 'mongodb://localhost:27017/flask_m3_project'
+database = PyMongo(app)
+
+
 
 from application import routes
