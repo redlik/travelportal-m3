e3f3330b34827a8fbba0d573205eb98ad14949be
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-25 07:58:37 +0100

    Testing heroku env variables

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 database = PyMongo(app)
 
