ee548585dc5d85fdc821216e677d959986f7edf0
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-25 11:27:55 +0000

    Removed instance config

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_pymongo import PyMongo
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 # app.config.from_pyfile('config.py')
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
