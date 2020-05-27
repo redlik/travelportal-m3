3fc15f05dfeafe291cf85764a7b4144d65565172
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-27 21:27:30 +0100

    Carousel mobile layout fixed

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
 
-  $(".owl-carousel").owlCarousel({
+    $(".owl-carousel").owlCarousel({
+    
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    items : 8,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [979,3],
+    center: false,
+    nav:true,
+    loop:true,
+    responsive: {
+      0:{ items:1 },
+      600:{ items:3 },
+      992:{ items:4 }
+    }
+    
+    });
   
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true,
-  items : 8,
-  itemsDesktop : [1199,3],
-  itemsDesktopSmall : [979,3],
-  center: false,
-  nav:true,
-  loop:true,
-  responsive: {
-  600: {
-  items: 4
-  }
-  }
-  
-  
-  
-  
-  
-  
-  });
-  
-  });
+});
